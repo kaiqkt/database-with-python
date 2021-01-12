ead152c7891c073ebf052a967a22091ed6647a5f
kaiquepill <kaique.gomes@bandtec.com.br>
2021-01-12 10:52:26 -0300

    sqlite3

--- bd/bdsqlite3.py
+++ bd/bdsqlite3.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conexao = sqlite3.connect('bd.db')
+conexao = sqlite3.connect('bd2.db')
 cursor = conexao.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS clientes ('
@@ -9,22 +9,22 @@ cursor.execute('CREATE TABLE IF NOT EXISTS clientes ('
                'peso REAL'
                ')')
 
-# cursor.execute('INSERT INTO clientes (nome, peso) VALUES (?, ?)', ('Maria', 45.5))
-# cursor.execute('INSERT INTO clientes VALUES (:id, :nome, :peso)',
-#                {'id': None, 'nome': 'Gabriel', 'peso': 125.0})
-# conexao.commit()
+cursor.execute('INSERT INTO clientes (nome, peso) VALUES (?, ?)', ('Maria', 45.5))
+cursor.execute('INSERT INTO clientes VALUES (:id, :nome, :peso)',
+               {'id': None, 'nome': 'Gabriel', 'peso': 125.0})
+conexao.commit()
 
-# cursor.execute(
-#     'UPDATE clientes SET nome=:nome WHERE id=:id',
-#     {'nome': 'Joana', 'id': 2}
-# )
-#
-# conexao.commit()
-#
-# cursor.execute(
-#     'DELETE FROM clientes WHERE id=:id',
-#     {'id': 2}
-# )
+cursor.execute(
+    'UPDATE clientes SET nome=:nome WHERE id=:id',
+    {'nome': 'Joana', 'id': 2}
+)
+
+conexao.commit()
+
+cursor.execute(
+    'DELETE FROM clientes WHERE id=:id',
+    {'id': 2}
+)
 
 conexao.commit()
 
